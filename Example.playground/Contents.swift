4dbcdf76018182e5f0310204645dc77c66ecb8f1
Sviatoslav Yakymiv <sviatoslav.yakymiv@gmail.com>
2018-10-02 23:51:34 +0300

    Update Example.playground.

--- Example.playground/Contents.swift
+++ Example.playground/Contents.swift
@@ -16,12 +16,59 @@
 
  The easiest way to create instance of `EndpointProcedure` is to creates type that conforms to `EndpointProcedureFactory` protocol.
  This example will show how to use `EndpointProcedure` for loading data form [Star Wars API](https://swapi.co).
- */
-import Foundation
 
-struct Film: Decodable {
+ Let's start from defining `struct Film`.
+ */
+import PlaygroundSupport
+import Foundation
+struct Film {
     let title: String
     let director: String
     let producer: String
     let characters: [URL]
 }
+/*:
+ For `EndpointProcedure` creation we should create new type that conforms to `EndpointProcedureFactory` protocol and implement `createOrThrow(with:)` method.
+ */
+struct FilmsEndpointProcedureFactory: EndpointProcedureFactory {
+    func createOrThrow(with configuration: ConfigurationProtocol) throws -> EndpointProcedure<[Film]> {
+        let url = URL(string: "https://swapi.co/api/films")!
+        let data = HTTPRequestData.Builder.for(url).build()
+        return EndpointProcedure(requestData: data, configuration: configuration)
+    }
+}
+/*:
+ `cereateOrThrow(with:)` method requires one input parameter of type `ConfigurationProtocol`. `EndpointProcedure.framework` contains struct `Configuration` which conforms to `ConfigurationProtocol`. Initializer of `Configuration` type has 3 input parameters: `HTTPDataLoadingProcedureFactory`, `DataDeserializationProcedureFactory` and `ResponseMappingProcedureFactory`.
+
+ We'll use `AlamofireProcedureFactory` for data loading.
+ */
+let loadingFactory = AlamofireProcedureFactory()
+/*:
+ For response mapping we'll use `DecodingProcedureFactory` with `JSONDecoder`.
+ `DecodingProcedureFactory` requires `Decodable` type.
+ */
+extension Film: Decodable {}
+let decodingFactory = DecodingProcedureFactory(decoder: JSONDecoder())
+/*:
+ The aim of data deserialization procedure is converting data loaded by data loading procedure into format expected by response mapping procedure.
+ `DecodingProcedure` accepts `Data` or `NestedData` input.
+ */
+let deseriazationFactory = AnyDataDeserializationProcedureFactory {
+    let codingPath: [AnyCodingKey] = ["results"]
+    return NestedData(codingPath: codingPath, data: $0)
+}
+
+let config = Configuration(dataLoadingProcedureFactory: loadingFactory,
+                           dataDeserializationProcedureFactory: deseriazationFactory,
+                           responseMappingProcedureFactory: decodingFactory)
+let procedure = FilmsEndpointProcedureFactory().create(with: config)
+procedure.addDidFinishBlockObserver { procedure, _ in
+    switch procedure.output {
+    case .pending: print("No result after finishing")
+    case .ready(.success(let films)): print(films)
+    case .ready(.failure(let error)): print(error)
+    }
+    PlaygroundPage.current.finishExecution()
+}
+ProcedureQueue.main.add(operation: procedure)
+PlaygroundPage.current.needsIndefiniteExecution = true
