51d5ee4699cd14effa55cfa426bedfd48391f9ad
Sviatoslav Yakymiv <sviatoslav.yakymiv@gmail.com>
2018-10-03 10:34:20 +0300

    Update Example.playground

--- Example.playground/Contents.swift
+++ Example.playground/Contents.swift
@@ -1,9 +1,8 @@
 /*:
  `EndpointProcedure` is flexible typesafe bridge between network and application model.
  It's based on [`ProcedureKit`](https://github.com/procedurekit/procedurekit) framework.
- `EndpointProcedure`.
 
- `EndpointProcedure` itself does not perform any requests, it chains procedures and transfers ouput of one procedure to the next one and uses output of last procedure as own output.
+ `EndpointProcedure` itself does not perform any requests and does not parse the response, it chains procedures and transfers ouput of one procedure to the next one and uses output of last procedure as own output.
 
  Data flow looks as follows:
 
@@ -29,15 +28,7 @@ struct Film {
 }
 /*:
  For `EndpointProcedure` creation we should create new type that conforms to `EndpointProcedureFactory` protocol and implement `createOrThrow(with:)` method.
- */
-struct FilmsEndpointProcedureFactory: EndpointProcedureFactory {
-    func createOrThrow(with configuration: ConfigurationProtocol) throws -> EndpointProcedure<[Film]> {
-        let url = URL(string: "https://swapi.co/api/films")!
-        let data = HTTPRequestData.Builder.for(url).build()
-        return EndpointProcedure(requestData: data, configuration: configuration)
-    }
-}
-/*:
+ 
  `cereateOrThrow(with:)` method requires one input parameter of type `ConfigurationProtocol`. `EndpointProcedure.framework` contains struct `Configuration` which conforms to `ConfigurationProtocol`. Initializer of `Configuration` type has 3 input parameters: `HTTPDataLoadingProcedureFactory`, `DataDeserializationProcedureFactory` and `ResponseMappingProcedureFactory`.
 
  We'll use `AlamofireProcedureFactory` for data loading.
@@ -45,28 +36,47 @@ struct FilmsEndpointProcedureFactory: EndpointProcedureFactory {
 let loadingFactory = AlamofireProcedureFactory()
 /*:
  For response mapping we'll use `DecodingProcedureFactory` with `JSONDecoder`.
- `DecodingProcedureFactory` requires `Decodable` type.
+ `DecodingProcedureFactory` requires output type to conform `Decodable` protocol.
  */
 extension Film: Decodable {}
 let decodingFactory = DecodingProcedureFactory(decoder: JSONDecoder())
 /*:
- The aim of data deserialization procedure is converting data loaded by data loading procedure into format expected by response mapping procedure.
- `DecodingProcedure` accepts `Data` or `NestedData` input.
+ The aim of data deserialization procedure is converting data loaded by loading procedure into format expected by response mapping procedure.
+ `DecodingProcedure` works with plain data, so deserialization should simply return output of data loading procedure.
  */
-let deseriazationFactory = AnyDataDeserializationProcedureFactory {
+let deseriazationFactory = AnyDataDeserializationProcedureFactory(syncDeserialization: {$0})
+/*:
+ If structure of input expected by mapping procedure is not the same as structure of deserialization procedure's output, we should implement interception procedure.
+ In our case, array of films is not root of the response json. It's under "results" key.
+ For such cases `DecodingProcedure` accepts input of type `NestedData`.
+ 
+ `NestedData` contains two values `codingPath: [CodingKey]` and `data: Data`
+ */
+let interceptionProcedure = TransformProcedure<Any, Any> {
+    guard let data = $0 as? Data else { throw ProcedureKitError.requirementNotSatisfied() }
     let codingPath: [AnyCodingKey] = ["results"]
-    return NestedData(codingPath: codingPath, data: $0)
+    return NestedData(codingPath: codingPath, data: data)
 }
-
 let config = Configuration(dataLoadingProcedureFactory: loadingFactory,
                            dataDeserializationProcedureFactory: deseriazationFactory,
                            responseMappingProcedureFactory: decodingFactory)
+
+struct FilmsEndpointProcedureFactory: EndpointProcedureFactory {
+    func createOrThrow(with configuration: ConfigurationProtocol) throws -> EndpointProcedure<[Film]> {
+        let url = URL(string: "https://swapi.co/api/films")!
+        let data = HTTPRequestData.Builder.for(url).build()
+        return EndpointProcedure(requestData: data,
+                                 interceptionProcedure: interceptionProcedure,
+                                 configuration: configuration)
+    }
+}
+
 let procedure = FilmsEndpointProcedureFactory().create(with: config)
 procedure.addDidFinishBlockObserver { procedure, _ in
     switch procedure.output {
     case .pending: print("No result after finishing")
-    case .ready(.success(let films)): print(films)
-    case .ready(.failure(let error)): print(error)
+    case .ready(.success(let films)): print((["Star Wars Films:"] + films.map({ $0.title })).joined(separator: "\n"))
+    case .ready(.failure(let error)): print("Error: \(error)")
     }
     PlaygroundPage.current.finishExecution()
 }
