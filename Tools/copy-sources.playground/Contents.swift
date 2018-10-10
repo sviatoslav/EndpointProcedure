5c582075454d750c74c0eca5a1458163c39a8022
Sviatoslav Yakymiv <sviatoslav.yakymiv@gmail.com>
2018-10-10 23:38:10 +0300

    Update tooling.

---
+++ Tools/copy-sources.playground/Contents.swift
@@ -0,0 +1,30 @@
+import Cocoa
+
+@discardableResult
+func shell(_ command: String) -> Int32 {
+    let task = Process()
+    task.launchPath = "/bin/sh"
+    task.arguments = ["-c", command]
+    task.launch()
+    task.waitUntilExit()
+    return task.terminationStatus
+}
+let examplesPath = FileManager.default.currentDirectoryPath + "/Examples"
+let playgrounds = (try? FileManager.default.contentsOfDirectory(atPath: examplesPath)
+    .filter({ $0.hasSuffix(".playground") })) ?? []
+playgrounds.forEach {
+    let playgroundSources = "Examples/\($0.replacingOccurrences(of: " ", with: "\\ "))/Sources"
+    let sourcesSubdirs = ["Core", "Alamofire", "Decoding"]
+    let podsSubdirs = ["Alamofire/Source", "ProcedureKit/Sources/ProcedureKit"]
+    shell("rm -rf \(playgroundSources)")
+    shell("mkdir \(playgroundSources)")
+    sourcesSubdirs.forEach {
+        shell("cp -a Sources/\($0)/. \(playgroundSources)")
+    }
+    podsSubdirs.forEach {
+        shell("cp -a Pods/\($0)/. \(playgroundSources)")
+    }
+    shell("rm -rf \(playgroundSources)/DispatchQueue+Alamofire.swift")
+    shell("cp -a Examples/Supporting\\ Files/ \(playgroundSources)")
+
+}
